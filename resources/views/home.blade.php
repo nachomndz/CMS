5ae007cf6b5520723831b9ab121155381c02edcb
nachomendez <ignaciomendezbg@hotmail.com>
2019-10-02 20:41:58 +0200

    pequeños cambios

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">Panel de control</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,7 +14,13 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    Te has logueado con éxito!
+
+
+
+
+
+
                 </div>
             </div>
         </div>
